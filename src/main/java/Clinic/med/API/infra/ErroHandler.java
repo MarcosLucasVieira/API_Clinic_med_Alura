a6fdc6c61d41d21143c80ed0e890267b7ff80964
Marcos Lucas Vieira <102480205+MarcosLucasVieira@users.noreply.github.com>
2024-10-13 18:29:49 -0300

    fix

--- src/main/java/Clinic/med/API/infra/ErroHandler.java
+++ src/main/java/Clinic/med/API/infra/ErroHandler.java
@@ -2,8 +2,9 @@ package Clinic.med.API.infra;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
@@ -12,5 +13,19 @@ public class ErroHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity Erro404(){
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler (MethodArgumentNotValidException.class)
+    public ResponseEntity Erro400(MethodArgumentNotValidException ex){
+        var erro = ex.getFieldErrors();
+
+        return ResponseEntity.badRequest().body(erro.stream().map(DataErroValidation::new).toList());
+    }
+
+    private record  DataErroValidation (String field, String message ){
+
+        public DataErroValidation (FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
     }
 }
