6395168c2768f03a133c8bc00fe3b425326ff7bd
Marcos Lucas Vieira <102480205+MarcosLucasVieira@users.noreply.github.com>
2024-10-15 09:36:16 -0300

    fix

---
+++ src/main/java/Clinic/med/API/domain/user/User.java
@@ -0,0 +1,23 @@
+package Clinic.med.API.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name ="user")
+@Entity(name = "User")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String password;
+}
--- src/main/java/Clinic/med/API/infra/exception/ErroHandler.java
+++ src/main/java/Clinic/med/API/infra/exception/ErroHandler.java
@@ -1,4 +1,4 @@
-package Clinic.med.API.infra;
+package Clinic.med.API.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
